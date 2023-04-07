19c5b2e8f1a38f2e9c798667e30134917fbe8b05
Christopher Peret <cperet89@gmail.com>
2023-04-07 15:27:23 -0700

    readMeGen.js fileswapped and app.js started

--- app.js
+++ app.js
@@ -1,39 +1,69 @@
-class markDown {
-    static generateMarkdown(data) {
-        return `
-        # ${data.title}
+const inquirer = require('inquirer');
+const fs = require('fs');
+const MarkDown = require('./lib/readMeGen.js');
 
-        ## Table of Contents
-        * [Installation](#installation)
-        * [Usage](#usage)
-        * [License](#license)
-        * [Contributing](#contributing)
-        * [Tests](#tests)
-        * [Questions](#questions)
-        
-        ## Description
-        ${data.description}
 
-        ## Installation
-        ${data.installation}
-        
-        ## Usage
-        ${data.usage}
-        
-        ## License
-        ${data.license}
-        
-        ## Contributing
-        ${data.contributing}
-        
-        ## Tests
-        ${data.tests}
-        
-        ## Questions
-        ${data.email}
-        ${data.github}
-        `;
-    }
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description of your project.',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide installation instructions for your project.',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide usage information for your project.',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Please provide contribution guidelines for your project.',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Please provide test instructions for your project.',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please select a license for your project.',
+        choices: ['MIT', 'Apache', 'GPL', 'BSD', 'None'],
+        filter(val) {
+            return val.toLowerCase();
+        }
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please provide your GitHub username.',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please provide your email address.',
+    },
+];
+
+function writeToFile(fileName, data) {
 }
 
-module.exports = markDown;
+function runQuery() {
+    return inquirer.prompt(questions)
+        .then((data) => {
+            const readMe = MarkDown.generateMarkdown(data);
+            fs.writeFile('README.md', readMe, (err) =>
+                err ? console.log(err) : console.log('Success!', data)
+            );
+        });
+}
