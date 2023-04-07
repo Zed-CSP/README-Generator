85f6977b643e53b370534151b1c35c58291d2381
Christopher Peret <cperet89@gmail.com>
2023-04-07 15:11:37 -0700

    readMeGen.js markdown class init

---
+++ app.js
@@ -0,0 +1,37 @@
+class markDown {
+    static generateMarkdown(data) {
+        return `
+        # ${data.title}
+
+        ## Table of Contents
+        * [Installation](#installation)
+        * [Usage](#usage)
+        * [License](#license)
+        * [Contributing](#contributing)
+        * [Tests](#tests)
+        * [Questions](#questions)
+        
+        ## Description
+        ${data.description}
+
+        ## Installation
+        ${data.installation}
+        
+        ## Usage
+        ${data.usage}
+        
+        ## License
+        ${data.license}
+        
+        ## Contributing
+        ${data.contributing}
+        
+        ## Tests
+        ${data.tests}
+        
+        ## Questions
+        ${data.email}
+        ${data.github}
+        `;
+    }
+}
